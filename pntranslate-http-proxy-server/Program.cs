6aa73af60675a57f20c86359999a2a31e5b5abe3
ug00031 <ug00031@surrey.ac.uk>
2020-12-10 17:53:48 +0000

    didn't reinvent the wheel this time, should let other requests pass while intercepting needed ones

--- pntranslate-http-proxy-server/Program.cs
+++ pntranslate-http-proxy-server/Program.cs
@@ -2,7 +2,13 @@
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using BenderProxy;
+using BenderProxy.Headers;
+using BenderProxy.Writers;
 using Microsoft.Win32;
+using HttpResponseHeader = BenderProxy.Headers.HttpResponseHeader;
 
 namespace pntranslate_http_proxy_server
 {
@@ -13,60 +19,40 @@ namespace pntranslate_http_proxy_server
 
         static void Main(string[] args)
         {
-            try
+            RegistryKey registry =
+                Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
+                    true);
+            registry.SetValue("ProxyEnable", 1);
+            registry.SetValue("ProxyServer", "localhost:8081");
+            HttpProxyServer httpProxyServer = new HttpProxyServer("localhost", 8081, new HttpProxy()
             {
-                _tcpListener = new TcpListener(IPAddress.Loopback, 8081);
-                _tcpListener.Start();
-                WebClient wc = new WebClient();
-                RegistryKey registry =
-                    Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
-                        true);
-                registry.SetValue("ProxyEnable", 1);
-                registry.SetValue("ProxyServer", "localhost:8081");
-                while (true)
+                OnResponseReceived = context =>
                 {
-                    TcpClient client = _tcpListener.AcceptTcpClient();
-                    Stream clientStream = client.GetStream();
-                    StreamReader clientStreamReader = new StreamReader(clientStream);
-                    String httpCmd = clientStreamReader.ReadLine();
-                    String[] splitBuffer = httpCmd.Split(" ", 3);
-                    String method = splitBuffer[0];
-                    String remoteUri = splitBuffer[1];
-                    Console.WriteLine($"{method} {remoteUri}");
-                    byte[] responseBytes;
-                    if (method.Equals("GET"))
+                    Console.WriteLine(context.RequestHeader.RequestURI);
+                    switch (context.RequestHeader.RequestURI)
                     {
-                        switch (remoteUri)
-                        {
-                            case "http://v1.ninjawars.ru/":
-                                responseBytes = File.ReadAllBytes("test.html");
-                                clientStream.Write(responseBytes, 0, responseBytes.Length);
-                                clientStream.Close();
-                                break;
-                            case "http://v1.ninjawars.ru/DM.swf":
-                                responseBytes = File.ReadAllBytes("DM.swf");
-                                clientStream.Write(responseBytes, 0, responseBytes.Length);
-                                clientStream.Close();
-                                registry.SetValue("ProxyEnable", 0);
-                                Environment.Exit(0);
-                                break;
-                            default:
-                                registry.SetValue("ProxyEnable", 0);
-                                responseBytes = wc.DownloadData(remoteUri);
-                                clientStream.Write(responseBytes, 0, responseBytes.Length);
-                                clientStream.Close();
-                                registry.SetValue("ProxyEnable", 1);
-                                break;
-                        }
+                        case "http://v1.ninjawars.ru/":
+                            SendFile("test.html", context, 250);
+                            break;
+                        case "http://v1.ninjawars.ru/DM.swf":
+                            SendFile("DM.swf", context, 500);
+                            registry.SetValue("ProxyEnable", 0);
+                            Environment.Exit(0);
+                            break;
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
-                registry.SetValue("ProxyEnable", 0);
-                throw;
-            }
+            });
+            httpProxyServer.Start();
+        }
+
+        static void SendFile(String fileName, ProcessingContext context, int timeout)
+        {
+            var bytesToWrite = File.ReadAllBytes(fileName);
+            var responseStream = new MemoryStream(bytesToWrite, false);
+            context.ResponseHeader.EntityHeaders.ContentLength = responseStream.Length;
+            Console.WriteLine(context.ResponseHeader);
+            new HttpResponseWriter(context.ClientStream).Write(context.ResponseHeader, responseStream, responseStream.Length);
+            Thread.Sleep(timeout);
         }
     }
 }
